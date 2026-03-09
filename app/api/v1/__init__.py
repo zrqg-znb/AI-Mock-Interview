cff80a991c199e5a6482e735d3906c1513aecf4d
张瑞卿 <1315993841@qq.com>
2026-03-09 23:18:52 +0800

    【app 初始化】

---
+++ app/api/v1/__init__.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+
+from app.core.dependency import DependPermission
+
+from .apis import apis_router
+from .auditlog import auditlog_router
+from .base import base_router
+from .depts import depts_router
+from .menus import menus_router
+from .roles import roles_router
+from .users import users_router
+
+v1_router = APIRouter()
+
+v1_router.include_router(base_router, prefix="/base")
+v1_router.include_router(users_router, prefix="/user", dependencies=[DependPermission])
+v1_router.include_router(roles_router, prefix="/role", dependencies=[DependPermission])
+v1_router.include_router(menus_router, prefix="/menu", dependencies=[DependPermission])
+v1_router.include_router(apis_router, prefix="/api", dependencies=[DependPermission])
+v1_router.include_router(depts_router, prefix="/dept", dependencies=[DependPermission])
+v1_router.include_router(auditlog_router, prefix="/auditlog", dependencies=[DependPermission])
